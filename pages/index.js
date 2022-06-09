d24b26e0807b631a2800043a073ecf3e71dc1fe2
Jacob Choi <jacob.j.choi@gmail.com>
2022-06-08 21:36:10 -0700

    finished lab38

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,44 @@
-import { useState } from 'react'
+import Head from 'next/head'
+import Header from '../components/Header'
+import CreateForm from '../components/CreateForm'
+import ReportTable from '../components/ReportTable'
+import Footer from '../components/Footer'
+import {hours} from './data'
+import {useState} from 'react'
 
-export default function Home() {
-  let [state, setState] = useState( {data: []});
-  return (
-    <div>
-      <Header />
-      <main className='flex flex-col items-center py-4 space-y-8'>
-        <CookieStandForm onSubmit={inputHandler} />
-        <Main data={state}/>
-        </main>
-      <Footer copywright= '2022' />
-    </div>
-  );
+export default function CookieStandAdmin() {
 
-  function inputHandler(event){
+  const [storeList, setStoreList] = useState([])
+  
+
+  function handleCreate(event) {
     event.preventDefault();
-    let store = {
-      Location : event.target.Location.value,
-      minCustomers: parseInt(event.target.MinimumPerHour.value),
-      maxCustomers: parseInt(event.target.MaximumPerHour.value),
-      avgCookies: parseFloat(event.target.AverageCookiesPerHour.value),
+    
+    const storeLists = {
+      location:event.target.location.value,
+      min_cust_per_hour:event.target.min_cust_per_hour.value,
+      max_cust_per_hour:event.target.max_cust_per_hour.value,
+      avg_cookies_per_sale: event.target.avg_cookies_per_sale.value,
+      hourly_sales: [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36],
+      id: storeList.length
     }
-    setState(store);
-    event.target.reset();
+
+    setStoreList([...storeList, storeLists])
   }
-}
-
-function Header(){
-  return <header className="px-8 py-6 text-4xl bg-emerald-500 text-black">
-    <h1>Cookie Stand Admin</h1>
-  </header>
-}
-
-function Main(props){
-  return(
-    <main className="flex flex-col items-center py-y pt-6 space-y-8">
-      <p className="text-sm text-gray-500">Report Table Coming Soon...</p>
-      <DisplayJson data={props.data} />
+  
+  return (
+    <div className="">
+      <Head>
+        <title>Cookie Stand Admin</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className='p-6 bg-emerald-50'>
+        <CreateForm handleCreate={handleCreate} />
+        <ReportTable hours={hours} storeList={storeList} /> 
       </main>
-  )
-}
-
-function CookieStandForm(props){
-    return (
-      <form className='grid grid-rows-2 w-1/2 p-2 bg-emerald-300 rounded-md my-10' onSubmit={props.onSubmit}>
-        <h1 className='flex justify-center py-3 text-2xl'>Create Cookie Stand</h1>
-        <div className="grid col-span-2">
-          <label>Location </label>
-          <input name='location' className='flex-auto w-full' placeholder='Location' required />
-        </div>
-        <div className='flex flex-row'>
-          <div className='grid col-span-'>
-          </div>
-          <div className='space-x-5'>
-            <label>Minimum Customers per Hour </label>
-            <input name='minCustHr' className='flex-bottom' placeholder='Minimum Customers per Hour' required />
-            <label>Maximum Customers per Hour </label>
-            <input name='maxCustHr' className='flex-auto' placeholder='Maximum Customers per Hour' required />
-            <label>Average Cookies per Sale </label>
-            <input name='avgCookiesSale' className='flex-auto' placeholder='Average Cookies per Sale' required />
-            <button className='px-4 py-2 bg-emerald-500 m-2'>Create</button>
-          </div>
-        </div>
-      </form>
-    )
-  }
-
-function DisplayJson(props) {
-  return (
-    <div>
-      {JSON.stringify(props.data) == '{"data":[]}' ? (
-        "Enter Cookie Data"
-      ) : (
-        <p className="text-sm tracking-widest text-gray-500">
-          {JSON.stringify(props.data)}
-        </p>
-      )}
+      <Footer totalLocations={storeList.length}/>
+      
     </div>
-  );
-}
-
-function Footer({ copywright }) {
-  return (
-    <footer className="px-8 py-6 bg-emerald-500 text-black">
-      <p>&copy;{copywright}</p>
-    </footer>
   )
 }
